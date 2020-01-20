0d1756e608428b656d1676f0a659e7e9e1a0cbca
WilliamRabuske <williamrabuske93@gmail.com>
2020-01-19 22:14:05 -0500

    completed v1.0

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/Fibonacci.java
@@ -1,33 +1,31 @@
 package com.techelevator;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Fibonacci {
 
 	public static void main(String[] args) {
 		try (Scanner in = new Scanner(System.in)) {
-
+			int x = 0;
+			int numberBefore = 0;
+			int numberAfter = 1;
 			System.out.println("Please insert a value:");
-			String value = in.nextLine();
-			int x = Integer.parseInt(value);
-			int[] fib = new int[x];
-			fib[0] = 0;
-			fib[1] = 1;
 
-			for (int i = 2; i < x - 1; i++)
+			x = in.nextInt();
+
+			for (int i = 1; i < x; i++)
 
 			{
-				fib[i] = fib[i - 1] + fib[i - 2];
-				if (fib[i] > x) {
-					int[] fibFinal = Arrays.copyOfRange(fib, 0, ((i + 1) - 1));
-					System.out.println(Arrays.toString(fibFinal));
-					break;
-				}
+				int sum = numberBefore + numberAfter;
+				numberBefore = numberAfter;
+				numberAfter = sum;
+				i = numberAfter;
+				System.out.print(numberBefore + " ");
 
 			}
 
 		}
 
 	}
+
 }
