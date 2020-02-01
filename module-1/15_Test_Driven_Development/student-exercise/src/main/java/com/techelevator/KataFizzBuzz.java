290b4518839c313d54228630180be68a9a1d1038
WilliamRabuske <williamrabuske93@gmail.com>
2020-02-01 16:12:26 -0500

    Add passing test method for testDivisibleby5And3ReturnFizzBuzz

--- module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -3,13 +3,18 @@ package com.techelevator;
 public class KataFizzBuzz {
 
 	public String fizzBuzz(int num) {
+
+		if (num % 3 == 0 && num % 5 == 0) {
+			return "FizzBuzz";
+
+		}
 		if (num % 3 == 0) {
 			return "Fizz";
 		}
 		if (num % 5 == 0) {
 			return "Buzz";
 		}
-		return "test";
 
+		return "test";
 	}
 }
