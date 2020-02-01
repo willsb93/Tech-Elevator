5a4aea7b830c88cb076e59896a5902d329d34db1
WilliamRabuske <williamrabuske93@gmail.com>
2020-02-01 15:54:57 -0500

    Add test method testDivisibleby3ReturnFizz

--- module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -4,5 +4,15 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataFizzBuzzTest {
+	
+	KataFizzBuzz acess = new KataFizzBuzz();
+	
+	@Test
+	
+	public void testDivisibleby3ReturnFizz() {
+		assertEquals("Fizz", acess);
+	}
+	
+	
 
 }
