a1e10db844dd525dcbd4657d2e4baa4c0542de53
WilliamRabuske <williamrabuske93@gmail.com>
2020-02-01 16:03:44 -0500

    Add test method testDivisibleBy5ReturnBuzz

--- module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/15_Test_Driven_Development/student-exercise/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -9,11 +9,19 @@ public class KataFizzBuzzTest {
 	
 	@Test
 	
-	public void testDivisibleby3ReturnFizz() {
+	public void testDivisibleBy3ReturnFizz() {
 		assertEquals("Fizz", acess.fizzBuzz(3));
 		assertEquals("Fizz", acess.fizzBuzz(9));
 		assertEquals("Fizz", acess.fizzBuzz(33));
 		assertEquals("Fizz", acess.fizzBuzz(12));
+	}
+	
+	@Test
+	public void testDivisibleBy5ReturnBuzz() {
+		assertEquals("Buzz", acess.fizzBuzz(5));
+		assertEquals("Buzz", acess.fizzBuzz(10));
+		assertEquals("Buzz", acess.fizzBuzz(20));
+		assertEquals("Buzz", acess.fizzBuzz(25));
 	}
 	
 	
