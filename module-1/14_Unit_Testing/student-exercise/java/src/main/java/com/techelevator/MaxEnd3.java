be19e2fd76f15c168c1d3ad6aefbe3bbea5b0453
WilliamRabuske <williamrabuske93@gmail.com>
2020-02-01 12:11:19 -0500

    Completed

--- module-1/14_Unit_Testing/student-exercise/java/src/main/java/com/techelevator/MaxEnd3.java
+++ module-1/14_Unit_Testing/student-exercise/java/src/main/java/com/techelevator/MaxEnd3.java
@@ -2,21 +2,20 @@ package com.techelevator;
 
 public class MaxEnd3 {
 
-    /*
-     Given an array of ints length 3, figure out which is larger between the first and last elements
-     in the array, and set all the other elements to be that value. Return the changed array.
-     MakeArray([1, 2, 3]) → [3, 3, 3]
-     MakeArray([11, 5, 9]) → [11, 11, 11]
-     MakeArray([2, 11, 3]) → [3, 3, 3]
-     */
-    public int[] makeArray(int[] nums) {
-        int largerNum = (nums[0] > nums[nums.length - 1]) ? nums[0] : nums[nums.length - 1];
+	/*
+	 * Given an array of ints length 3, figure out which is larger between the first
+	 * and last elements in the array, and set all the other elements to be that
+	 * value. Return the changed array. MakeArray([1, 2, 3]) → [3, 3, 3]
+	 * MakeArray([11, 5, 9]) → [11, 11, 11] MakeArray([2, 11, 3]) → [3, 3, 3]
+	 */
+	public int[] makeArray(int[] nums) {
+		int larger = Math.max(nums[0], nums[2]); // Kept having problems here, decided to rewrite with Math.max
+		int largerNum = Math.max(nums[1], larger);
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = largerNum;
+		}
 
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = largerNum;
-        }
-
-        return nums;
-    }
+		return nums;
+	}
 
 }
--- module-1/14_Unit_Testing/student-exercise/java/src/test/java/com/techelevator/CigarPartyTest.java
+++ module-1/14_Unit_Testing/student-exercise/java/src/test/java/com/techelevator/CigarPartyTest.java
@@ -1,5 +1,34 @@
 package com.techelevator;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Assert.*;
+
 public class CigarPartyTest {
 
+	CigarParty cigar = new CigarParty();
+
+	@Test
+	public void testWekeendFalse() {
+		Assert.assertEquals(false, cigar.haveParty(35, false));
+		Assert.assertEquals(false, cigar.haveParty(65, false));
+		Assert.assertEquals(true, cigar.haveParty(50, false));
+		Assert.assertEquals(true, cigar.haveParty(60, false));
+		Assert.assertEquals(true, cigar.haveParty(40, false));
+	}
+
+	@Test
+	public void testWekeendTrue() {
+		Assert.assertEquals(true, cigar.haveParty(40, true));
+		Assert.assertEquals(true, cigar.haveParty(65, true));
+		Assert.assertEquals(false, cigar.haveParty(35, true));
+		Assert.assertEquals(false, cigar.haveParty(25, true));
+		Assert.assertEquals(true, cigar.haveParty(90, true));
+	}
 }
