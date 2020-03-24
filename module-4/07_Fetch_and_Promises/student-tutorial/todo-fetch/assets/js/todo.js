2b525ea7b164db7b4ba7d309ef3fc368adad25ee
WilliamRabuske <williamrabuske93@gmail.com>
2020-03-24 17:45:03 -0400

    tutorial completed

--- module-4/07_Fetch_and_Promises/student-tutorial/todo-fetch/assets/js/todo.js
+++ module-4/07_Fetch_and_Promises/student-tutorial/todo-fetch/assets/js/todo.js
@@ -1,2 +1,23 @@
-
+fetch('data.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((todos) => {
+    if('content' in document.createElement('template')) {
+      const list = document.querySelector("ul");
+      todos.forEach((todo) => {
+        const tmpl = document.getElementById('tasks').content.cloneNode(true);
+        tmpl.querySelector("li").insertAdjacentHTML('afterbegin',todo.task);
+        if( todo.completed ) {
+          const circleCheck = tmpl.querySelector('.fa-check-circle');
+          const currentClass = circleCheck.className;
+          circleCheck.className = currentClass + " completed";
+        }
+        list.appendChild(tmpl);
+      });
+    } else {
+      console.error('Your browser does not support templates');
+    }
+  })
+  .catch((err) => {console.error(err)});
 
