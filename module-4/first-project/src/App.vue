1ca0e6204b6cb09c83304912712566acfe551818
WilliamRabuske <williamrabuske93@gmail.com>
2020-03-26 11:36:19 -0400

    --

--- module-4/first-project/src/App.vue
+++ module-4/first-project/src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'>
+    <product-review> </product-review>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import productReview from './components/productReview.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    productReview
   }
 }
 </script>
---
+++ module-4/first-project/src/components/productReview.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id ='app'>
+        <h2> Product Reviews for </h2>
+        <p> This is the description </p>
+    
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'product-review',
+    data() {
+        return {
+            name: 'Cigar Party For Dummies',
+            description: 'This is my cigar party reviews'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
