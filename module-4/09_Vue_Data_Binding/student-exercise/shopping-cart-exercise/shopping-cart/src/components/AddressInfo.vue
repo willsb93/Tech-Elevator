aa59121c1f011c18bb2ac4f66b01e197debccaa9
WilliamRabuske <williamrabuske93@gmail.com>
2020-03-27 18:15:20 -0400

    done

--- module-4/09_Vue_Data_Binding/student-exercise/shopping-cart-exercise/shopping-cart/src/components/AddressInfo.vue
+++ module-4/09_Vue_Data_Binding/student-exercise/shopping-cart-exercise/shopping-cart/src/components/AddressInfo.vue
@@ -8,7 +8,13 @@
           </div>
           <div class="form-group">
             <label for="shippingAddress">Address</label>
-            <input type="text" class="form-control" id="shippingAddress" placeholder="1234 Main St" />
+            <input
+              type="text"
+              class="form-control"
+              id="shippingAddress"
+              placeholder="123 Your St"
+              v-model.trim="shipping.address"
+            >
           </div>
           <div class="form-group">
             <label for="shippingAddress2">Address 2</label>
@@ -16,31 +22,35 @@
               type="text"
               class="form-control"
               id="shippingAddress2"
-              placeholder="Apartment, studio, or floor"
-            />
+              placeholder="Apartment"
+              v-model.trim="shipping.address2"
+            >
           </div>
           <div class="form-row">
             <div class="form-group col-md-6">
               <label for="shippingCity">City</label>
-              <input type="text" class="form-control" id="shippingCity" />
+              <input type="text" class="form-control" id="shippingCity" v-model.trim="shipping.city">
             </div>
             <div class="form-group col-md-4">
               <label for="shippingState">State</label>
-              <select id="shippingState" class="form-control">
-                <option value selected>Choose...</option>
-                <option value="MI">MI</option>
+              <select id="shippingState" class="form-control" v-model.trim="shipping.state">
+                <option value="" selected>Choose</option>
+                <option value="AL">AL</option>
                 <option value="OH">OH</option>
                 <option value="PA">PA</option>
+                <option value="CA">CA</option>
+                <option value="IL">IL</option>
+                <option value="WY">WY</option>
               </select>
             </div>
             <div class="form-group col-md-2">
               <label for="shippingZip">Zip</label>
-              <input type="text" class="form-control" id="shippingZip" />
+              <input type="text" class="form-control" id="shippingZip" v-model.trim="shipping.zip">
             </div>
           </div>
           <div class="form-row">
             <div class="form-group col-md-4">
-              <label for="deliverto">Deliver Package to:</label>
+              <label for="deliverto">Deliver to:</label>
               <div class="form-check">
                 <input
                   class="form-check-input"
@@ -48,8 +58,9 @@
                   name="deliverto"
                   id="delivertoFrontDoor"
                   value="Front Door"
+                  v-model.trim="shipping.deliverto"
                   checked
-                />
+                >
                 <label class="form-check-label" for="delivertoFrontDoor">Front Door</label>
               </div>
               <div class="form-check">
@@ -59,7 +70,8 @@
                   name="deliverto"
                   id="delivertoSideDoor"
                   value="Side Door"
-                />
+                  v-model.trim="shipping.deliverto"
+                >
                 <label class="form-check-label" for="delivertoSideDoor">Side Door</label>
               </div>
               <div class="form-check">
@@ -69,13 +81,27 @@
                   name="deliverto"
                   id="delivertoBackDoor"
                   value="Back Door"
-                />
+                  v-model.trim="shipping.deliverto"
+                >
                 <label class="form-check-label" for="delivertoBackDoor">Back Door</label>
               </div>
             </div>
           </div>
 
-          <div class="billing-add">
+          <div class="form-group">
+            <div class="form-check">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                id="billingSameCheckbox"
+                :checked="billingSameAsShipping"
+                v-on:change="updateBilling"
+              >
+              <label class="form-check-label" for="billingSameCheckbox">Billing same as shipping</label>
+            </div>
+          </div>
+
+          <div class="billing-add" v-if="!billingSameAsShipping">
             <div class="form-row">
               <h2>Billing Address</h2>
             </div>
@@ -85,8 +111,9 @@
                 type="text"
                 class="form-control"
                 id="billingAddress"
-                placeholder="1234 Main St"
-              />
+                placeholder="123 Your St"
+                v-model.trim="billing.address"
+              >
             </div>
             <div class="form-group">
               <label for="billingAddress2">Address 2</label>
@@ -94,26 +121,30 @@
                 type="text"
                 class="form-control"
                 id="billingAddress2"
-                placeholder="Apartment, studio, or floor"
-              />
+                placeholder="Apartment"
+                v-model.trim="billing.address2"
+              >
             </div>
             <div class="form-row">
               <div class="form-group col-md-6">
                 <label for="billingCity">City</label>
-                <input type="text" class="form-control" id="billingCity" />
+                <input type="text" class="form-control" id="billingCity" v-model.trim="billing.city">
               </div>
               <div class="form-group col-md-4">
                 <label for="billingState">State</label>
-                <select id="billingState" class="form-control">
-                  <option value selected>Choose...</option>
-                  <option value="MI">MI</option>
-                  <option value="OH">OH</option>
-                  <option value="PA">PA</option>
+                <select id="billingState" class="form-control" v-model.trim="billing.state">
+                  <option value="" selected>Choose</option>
+                <option value="AL">AL</option>
+                <option value="OH">OH</option>
+                <option value="PA">PA</option>
+                <option value="CA">CA</option>
+                <option value="IL">IL</option>
+                <option value="WY">WY</option>
                 </select>
               </div>
               <div class="form-group col-md-2">
                 <label for="billingZip">Zip</label>
-                <input type="text" class="form-control" id="billingZip" />
+                <input type="text" class="form-control" id="billingZip" v-model.trim="billing.zip">
               </div>
             </div>
           </div>
@@ -126,63 +157,99 @@
           <h3>Shipping Address</h3>
           <div class="line-item">
             Address:
-            <span class="shipping-address"></span>
+            <span class="shipping-address">{{shipping.address}}</span>
           </div>
           <div class="line-item">
             Address 2:
-            <span class="shipping-address2"></span>
+            <span class="shipping-address2">{{shipping.address2}}</span>
           </div>
           <div class="line-item">
             City:
-            <span class="shipping-city"></span>
+            <span class="shipping-city">{{shipping.city}}</span>
           </div>
           <div class="line-item">
             State:
-            <span class="shipping-state"></span>
+            <span class="shipping-state">{{shipping.state}}</span>
           </div>
           <div class="line-item">
             Zip:
-            <span class="shipping-zip"></span>
+            <span class="shipping-zip">{{shipping.zip}}</span>
           </div>
           <div class="line-item">
             Deliver to:
-            <span class="shipping-deliverto"></span>
+            <span class="shipping-deliverto">{{shipping.deliverto}}</span>
           </div>
 
           <h3>Billing Address</h3>
           <div class="line-item">
             Address:
-            <span class="billing-address"></span>
+            <span class="billing-address">{{billing.address}}</span>
           </div>
           <div class="line-item">
             Address 2:
-            <span class="billing-address2"></span>
+            <span class="billing-address2">{{billing.address2}}</span>
           </div>
           <div class="line-item">
             City:
-            <span class="billing-city"></span>
+            <span class="billing-city">{{billing.city}}</span>
           </div>
           <div class="line-item">
             State:
-            <span class="billing-state"></span>
+            <span class="billing-state">{{billing.state}}</span>
           </div>
           <div class="line-item">
             Zip:
-            <span class="billing-zip"></span>
+            <span class="billing-zip">{{billing.zip}}</span>
           </div>
+
         </div>
       </div>
     </div>
   </div>
 </template>
 
+
 <script>
 export default {
   name: "address-info",
   data() {
-    return {};
+    return {
+      billingSameAsShipping: false,
+      shipping: {
+        address: "",
+        address2: "",
+        city: "",
+        state: "",
+        zip: "",
+        deliverto: ""
+      },
+      billing: {
+        address: "",
+        address2: "",
+        city: "",
+        state: "",
+        zip: ""
+      }
+    };
+  },
+  methods: {
+    updateBilling() {
+      this.billingSameAsShipping = !this.billingSameAsShipping;
+      this.billingSameAsShipping ? this.billing = this.shipping : this.clearBillingAddress();
+    },
+    clearBillingAddress() {
+      this.billing = {
+        address: "",
+        address2: "",
+        city: "",
+        state: "",
+        zip: ""
+      }
+    }
   }
-};
+    }
+  
+
 </script>
 
 <style>
