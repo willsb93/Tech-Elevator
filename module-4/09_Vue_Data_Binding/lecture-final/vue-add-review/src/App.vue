c0c3f3c16dbdd08c25a8e453af84d94f7d7887c7
Steve Carmichael <steve@techelevator.com>
2020-03-27 12:27:55 -0400

    added lecture code, exercise, and event handling tutorial

---
+++ module-4/09_Vue_Data_Binding/lecture-final/vue-add-review/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <add-review/>
+  </div>
+</template>
+
+<script>
+import AddReview from './components/AddReview.vue'
+
+export default {
+  name: 'app',
+  components: {
+    AddReview
+  }
+}
+</script>
+
+<style>
+</style>
