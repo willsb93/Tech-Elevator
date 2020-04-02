1121c507e4357281f4933a74b07933c448745a33
WilliamRabuske <williamrabuske93@gmail.com>
2020-04-02 17:35:29 -0400

    tutorial in progress

--- module-4/12_Vue_Router/student-tutorial/todo-router/src/router.js
+++ module-4/12_Vue_Router/student-tutorial/todo-router/src/router.js
@@ -1,17 +1,35 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/views/Home.vue";
+import Personal from "@/views/Personal";
+import Work from "@/views/Work";
+import House from "@/views/House";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
+    },
+    {
+      path: "/personal",
+      name: "personal",
+      component: Personal
+    },
+    {
+      path: "/work",
+      name: "work",
+      component: Work
+    },
+    {
+      path: "/house",
+      name: "house",
+      component: House
     }
   ]
-})
+});
---
+++ module-4/12_Vue_Router/student-tutorial/todo-router/src/views/Personal.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="personal">
+    <h1>Personal</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "personal",
+    components: {}
+  };
+</script>
